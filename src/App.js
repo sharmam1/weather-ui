7aa952ac8fed1894bc7c6386130ad55845ddf12c
Manish Sharma <smanish@athenahealth.com>
2021-10-06 11:30:07 +0530

    App main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React , {useState} from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+
+  const [textVal, setTextVal] = useState("Melbourne,AU");
+  const [response, setResponse] = useState('');
+  const [error, setError] = useState('');
+
+  // Handle form submit event.
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    // callin an external API 
+   axios.get(`http://localhost:8080/weather?city=${textVal}`, {}).
+      then(response => {
+        setResponse(response.data);
+      }).catch(error => {
+        setError("Not found");
+        setResponse('');
+    })
+  }
+
+  // handle input change for controlled form.
+  const handleInputChange = event => {
+    event.preventDefault();
+    setTextVal(event.target.value);
+  }
+
+  // UI rendering.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <form onSubmit={handleSubmit} > 
+      <label>
+        Enter city    
+        <input type="text" value = {textVal} onChange={handleInputChange}/>
+        </label>
+        <input type="submit" />
+     </form>
+      <div> 
+      Temprature: {response ? response.temp : 'Not found'}<br/>
+      Description : {response ? response.description : 'Not found'}
+      </div>
     </div>
   );
 }
